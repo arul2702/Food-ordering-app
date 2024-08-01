29027eb4af4dd2d5b0402db5b68fe1374c43a395
arul2702 <arulkarthi.p2021csea@sece.ac.in>
2024-08-01 21:45:34 +0530

    second

--- foodOrderSystem/src/Components/Navbar/Navbar.jsx
+++ foodOrderSystem/src/Components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = ({setShowLogin}) => {
   return (
 
     <div className='navbar'>
-        <img className='logo' src={assets.logo} alt="" />
+        <Link to='/'><img className='logo' src={assets.logo} alt="" /></Link>
         <ul className="navbar-menu">
             <Link to="/" onClick={()=>handleMenu("home")} className={menu==="home"?"active":""}>home</Link>
             <a href='#explore-menu' onClick={()=>handleMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
@@ -22,7 +22,7 @@ const Navbar = ({setShowLogin}) => {
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
+                <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
                 <div className="dot"></div>
             </div>
             <button onClick={()=>setShowLogin(true)}>sign in</button>
